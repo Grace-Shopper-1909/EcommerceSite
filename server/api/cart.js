a8ccfb95710df8144606828a5be1e1a53f98a68b
Olesia Guidi <olgarbuzova@gmail.com>
2019-11-06 15:57:20 -0500

    route get all products in cart works

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,21 +1,52 @@
 const router = require('express').Router()
-const {Cart, User, Product} = require('../db/models')
+const {Cart} = require('../db/models')
 
 // gets all producs in the cart for specified user:
 
 router.get('/', async (req, res) => {
-  try {
-    const allProducts = await Cart.findAll({
-      where: {userId: req.body.userId, isPurchasd: false}
-    })
-    if (!allProducts) {
-      res.status(404).send('No items in your cart!')
-    } else {
-      res.send(allProducts)
+  const user = req.params.userId
+  if (!user) {
+    res.send('no user defined!')
+  } else {
+    try {
+      const allProducts = await Cart.findAll({
+        where: {userId: user, isPurchasd: false}
+      })
+      if (!allProducts) {
+        res.status(404).send('No items in your cart!')
+      } else {
+        res.send(allProducts)
+      }
+    } catch (error) {
+      console.log(error)
     }
-  } catch (error) {
-    console.log(error)
   }
 })
 
-module.exports = cart
+// deletes item by id from the cart for specified user:
+
+router.delete('/:id', (req, res, next) => {
+  Cart.destroy({
+    where: {
+      id: req.params.Id,
+      userId: req.params.userId
+    }
+  })
+    .then(() => res.status(204).end())
+    .catch(next)
+})
+
+// creates new row in the cart with all params that's specified on req.body
+router.post('/', async (req, res) => {
+  try {
+    const newCart = await Cart.create(req.body)
+    res.json(newCart)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+// updates the cart (to change the quantity of item)
+router.put('/', async (req, res) => {})
+
+module.exports = router
--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-
+//delete this later:
+router.use('/cart', require('./cart'))
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
