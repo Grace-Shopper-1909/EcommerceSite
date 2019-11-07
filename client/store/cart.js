0a2b18058ed2e02aa1b711bc3f4fceb8144b0e6e
Emma Munley <emmamunley@gmail.com>
2019-11-07 11:10:05 -0500

    fixed: css

--- client/store/cart.js
+++ client/store/cart.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 /**
  * ACTION TYPES
  */
-// const GOT_PRODUCTS = 'GOT_PRODUCTS'
+const GOT_CART = 'GOT_CART'
 
 /**
  * INITIAL STATE
@@ -13,29 +13,31 @@ const initalState = []
 /**
  * ACTION CREATORS
  */
-// const gotProducts = products => ({
-//   type: GOT_PRODUCTS,
-//   products
-// })
+const gotCart = cart => ({
+  type: GOT_CART,
+  cart
+})
 
 /**
  * THUNK CREATORS
  */
-// export const getProducts = () => async dispatch => {
-//   try {
-//     const res = await axios.get('/api/products/')
-//     console.log('response', res)
-//     dispatch(gotProducts(res.data))
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
+export const getCart = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/<cart>')
+    console.log('response', res)
+    dispatch(gotCart(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 /**
  * REDUCER
  */
 export default function(state = initalState, action) {
   switch (action.type) {
+    case GOT_CART:
+      return action.cart
     default:
       return state
   }
