72701bef268ce91205e0945246eeea29b335a803
Mallory LeeWong <m.c.leewong@gmail.com>
2019-11-07 12:43:16 -0500

    refactor:  added more actions thunks reducer cases in cart store

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,6 +4,9 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GOT_CART = 'GOT_CART'
+const DELETED_PRODUCT = 'DELETED_PRODUCT'
+const ADDED_PRODUCT = 'ADDED_PRODUCT'
+const UPDATED_PRODUCT = 'UPDATED_PRODUCT'
 
 /**
  * INITIAL STATE
@@ -18,14 +21,65 @@ const gotCart = cart => ({
   cart
 })
 
+const deletedProduct = productId => ({
+  type: DELETED_PRODUCT,
+  productId
+})
+
+const addedProduct = (productId, product) => ({
+  type: ADDED_PRODUCT,
+  productId,
+  product
+})
+
+const updatedProduct = (productId, product) => ({
+  type: UPDATED_PRODUCT,
+  productId,
+  product
+})
+
 /**
  * THUNK CREATORS
  */
 export const getCart = () => async dispatch => {
   try {
-    const res = await axios.get('/api/<cart>')
-    console.log('response', res)
-    dispatch(gotCart(res.data))
+    const res = await axios.get('/api/cart')
+    console.log('axios data get', res)
+    return dispatch(gotCart(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const deleteProduct = productId => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/cart${productId}`)
+    console.log('axios data delete', res)
+    return dispatch(deletedProduct(productId))
+  } catch (err) {
+    console.error(err)
+  }
+}
+// this seems like it should be like the delete because it will be a button
+// need to grab the Id of the product associated with its add to cart button
+// add that product to the cart by producId
+// return the productId and the new product object
+// how does this work with magic methods? setProduct?
+export const addProduct = productId => async dispatch => {
+  try {
+    const res = await axios.post(`/api/cart${productId}`)
+    console.log('axios data add', res)
+    return dispatch(addedProduct(productId, res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const updateProduct = productId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/cart${productId}`)
+    console.log('axios data add', res)
+    return dispatch(updatedProduct(productId, res.data))
   } catch (err) {
     console.error(err)
   }
@@ -34,10 +88,19 @@ export const getCart = () => async dispatch => {
 /**
  * REDUCER
  */
-export default function(state = initalState, action) {
+export default function cart(state = initalState, action) {
   switch (action.type) {
     case GOT_CART:
-      return action.cart
+      return [...state, action.cart]
+    case DELETED_PRODUCT:
+      return [
+        ...state,
+        ...state.filter(product => product.id !== action.productId)
+      ]
+    case ADDED_PRODUCT:
+      return [...state, action.product]
+    case UPDATED_PRODUCT:
+      return [...state, action.product]
     default:
       return state
   }
